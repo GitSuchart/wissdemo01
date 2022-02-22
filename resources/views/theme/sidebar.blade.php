c387f88a2c58bc28ca249acde66030a3b9090e05
gitsuchart <suchart_au@aisin-ap.com>
2022-02-22 16:07:23 +0700

    commit

---
+++ resources/views/theme/sidebar.blade.php
@@ -0,0 +1,202 @@
+       <!-- Sidebar -->
+       <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+           <!-- Sidebar - Brand -->
+           <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+               <div class="sidebar-brand-icon rotate-n-15">
+                   <i class="fas fa-laugh-wink"></i>
+               </div>
+               <div class="sidebar-brand-text mx-3">WISS <sup>2</sup></div>
+           </a>
+
+           <!-- Divider -->
+           <hr class="sidebar-divider my-0">
+
+{{-- ========================================================================== --}}
+{{-- DASHBOARD --}}
+{{-- ========================================================================== --}}
+
+           <!-- Nav Item - Dashboard -->
+           <li class="nav-item active">
+               <a class="nav-link" href="index.html">
+                   <i class="fas fa-fw fa-tachometer-alt"></i>
+                   <span>Dashboard</span></a>
+           </li>
+
+           <!-- Divider -->
+           <hr class="sidebar-divider">
+{{-- ========================================================================== --}}
+{{-- BUSINESS APP --}}
+{{-- ========================================================================== --}}
+           <!-- Heading -->
+           <div class="sidebar-heading">
+               Business App Reports
+           </div>
+
+           <!-- Nav Item - Pages Collapse Menu -->
+           <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Admin Template</span>
+            </a>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="main">Main - Pare</a>
+                     <a class="collapse-item" href="basic-report">Basic Report</a>
+                     <a class="collapse-item" href="basic-report-api">Basic Report API</a>
+                     <a class="collapse-item" href="event-log">Event Log</a>
+                     <a class="collapse-item" href="buttons.html">SAP Invoice Interface</a>
+                     <a class="collapse-item" href="cards.html">SAP PO Interface</a>
+                     <a class="collapse-item" href="chart">Suchart</a>
+                </div>
+            </div>
+        </li>
+
+           <!-- Nav Item - Pages Collapse Menu -->
+           <li class="nav-item">
+               <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                   <i class="fas fa-fw fa-cog"></i>
+                   <span>EPS</span>
+               </a>
+               <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                   <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="buttons.html">Error Log</a>
+                        <a class="collapse-item" href="cards.html">Event Log</a>
+                        <a class="collapse-item" href="buttons.html">SAP Invoice Interface</a>
+                        <a class="collapse-item" href="cards.html">SAP PO Interface</a>
+                   </div>
+               </div>
+           </li>
+
+           <!-- Nav Item - Utilities Collapse Menu -->
+           <li class="nav-item">
+               <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                   <i class="fas fa-fw fa-wrench"></i>
+                   <span>E-MFG</span>
+               </a>
+               <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                   <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="buttons.html">PDS Interface</a>
+                        <a class="collapse-item" href="cards.html">ATAC - E-mfg Log</a>
+                        <a class="collapse-item" href="buttons.html">SA - E-mfg Log</a>
+                   </div>
+               </div>
+           </li>
+
+           <!-- Divider -->
+           <hr class="sidebar-divider">
+{{-- ========================================================================== --}}
+{{-- INFRA --}}
+{{-- ========================================================================== --}}
+           <!-- Heading -->
+           <div class="sidebar-heading">
+               Infra Reports
+           </div>
+
+           <!-- Nav Item - Pages Collapse Menu -->
+           <li class="nav-item">
+               <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseInfra" aria-expanded="true" aria-controls="collapseInfra">
+                   <i class="fas fa-fw fa-folder"></i>
+                   <span>ATGN</span>
+               </a>
+               <div id="collapseInfra" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="buttons.html">SA</a>
+                    <a class="collapse-item" href="cards.html">ATAC</a>
+                    <a class="collapse-item" href="buttons.html">AIAP</a>
+                    <a class="collapse-item" href="cards.html">AISIN Group</a>
+               </div>
+               </div>
+           </li>
+
+           <!-- Nav Item - Charts -->
+           <li class="nav-item">
+               <a class="nav-link" href="charts.html">
+                   <i class="fas fa-fw fa-chart-area"></i>
+                   <span>Zabbix</span></a>
+           </li>
+
+           <!-- Nav Item - Tables -->
+           <li class="nav-item">
+               <a class="nav-link" href="tables.html">
+                   <i class="fas fa-fw fa-table"></i>
+                   <span>O365</span></a>
+           </li>
+
+
+           <!-- Nav Item - Tables -->
+           <li class="nav-item">
+            <a class="nav-link" href="tables.html">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Network Monitoring</span></a>
+        </li>
+
+           <!-- Divider -->
+           <hr class="sidebar-divider d-none d-md-block">
+
+
+
+
+{{-- ========================================================================== --}}
+{{-- SAMPLE  --}}
+{{-- ========================================================================== --}}
+           <!-- Heading -->
+           <div class="sidebar-heading">
+               Sample Template
+           </div>
+
+
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Pages</span>
+            </a>
+            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Login Screens:</h6>
+                    <a class="collapse-item" href="theme/login.html">Login</a>
+                    <a class="collapse-item" href="theme/register.html">Register</a>
+                    <a class="collapse-item" href="theme/forgot-password.html">Forgot Password</a>
+                    <div class="collapse-divider"></div>
+                    <h6 class="collapse-header">Other Pages:</h6>
+                    <a class="collapse-item" href="theme/404.html">404 Page</a>
+                    <a class="collapse-item" href="theme/blank.html">Blank Page</a>
+                </div>
+            </div>
+        </li>
+
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="theme/charts.html">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Charts</span></a>
+        </li>
+
+        <!-- Nav Item - Tables -->
+        <li class="nav-item active">
+            <a class="nav-link" href="theme/tables.html">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Tables</span></a>
+        </li>
+
+{{-- ========================================================================== --}}
+{{-- TOGGER --}}
+{{-- ========================================================================== --}}
+                   <!-- Sidebar Toggler (Sidebar) -->
+                   <div class="text-center d-none d-md-inline">
+                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
+
+           {{-- <!-- Sidebar Message -->
+           <div class="sidebar-card d-none d-lg-flex">
+               <img class="sidebar-card-illustration mb-2" src="theme/img/undraw_rocket.svg" alt="...">
+               <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
+               <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
+           </div> --}}
+
+        </ul>
+        <!-- End of Sidebar -->
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
